45f45dae88470116672f4d16c04b92cd8eee7f50
akradimo <myparstech.ir@gmail.com>
2025-05-29 01:48:45 +0400

    02

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -7,9 +7,10 @@ use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 class Tenant extends BaseTenant
 {
     protected $casts = [
-        'data' => 'array'
+        'data' => 'array',
     ];
 
+    // اگر بعداً خواستی ارتباط با User بزنی
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
