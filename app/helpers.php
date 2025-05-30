e3dab8d3a92cecfbd25634632525c5a2d1de8354
akradimo <myparstech.ir@gmail.com>
2025-05-30 22:38:32 +0400

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,12 +1,17 @@
 <?php
 
-if (!function_exists('categoryTypeFa')) {
-    function categoryTypeFa($type)
+if (!function_exists('fa_number')) {
+    /**
+     * تبدیل عدد انگلیسی به فارسی با جداکننده (هزارگان و اعشاری)
+     * @param string|float|int $number
+     * @param int $decimals
+     * @return string
+     */
+    function fa_number($number, $decimals = 0)
     {
-        return [
-            'person' => 'اشخاص',
-            'product' => 'کالا',
-            'service' => 'خدمات',
-        ][$type] ?? $type;
+        $en = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ','];
+        $fa = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '٫', '٬'];
+        $number = number_format($number, $decimals, '.', ',');
+        return str_replace($en, $fa, $number);
     }
 }
