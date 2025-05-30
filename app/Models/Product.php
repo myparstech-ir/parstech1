923e356a348af9d9ea27cbaa65bcc9ec27a7e775
akradimo <myparstech.ir@gmail.com>
2025-05-30 21:19:51 +0400

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $guarded = [];
     use HasFactory;
 
     // مقدار پیش فرض هشدار موجودی
@@ -53,6 +54,16 @@ class Product extends Model
     public function getGalleryAttribute($value)
     {
         return $value ? json_decode($value, true) : [];
+    }
+    public function shareholders()
+    {
+        // اگر در جدول product_shareholder ستون person_id داری:
+        return $this->belongsToMany(Person::class, 'product_shareholder', 'product_id', 'person_id')
+            ->withPivot('percent');
+
+        // اگر مدل Shareholder داری و فیلدش shareholder_id است:
+        // return $this->belongsToMany(Shareholder::class, 'product_shareholder', 'product_id', 'shareholder_id')
+        //     ->withPivot('percent');
     }
 
     // اگر نیاز به متدهای بیشتری بود اینجا اضافه کن
